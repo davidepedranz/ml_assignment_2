3afb439cada00da8725fa9e46cf708a0621ba12b
Davide Pedranz <pedranz@fbk.eu>
2016-12-08 16:28:54 +0100

    Add Neural Network

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import matplotlib
 
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import make_classification
@@ -9,6 +9,7 @@ from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
 
 
 def generate_data(seed):
@@ -17,22 +18,23 @@ def generate_data(seed):
     :param seed: Seed to use for the random generator.
     :return: Pair (X, y) of features and classes.
     """
-    m = 1000
-    X, y = make_classification(n_samples=m, n_features=20, n_informative=10, n_redundant=3, n_repeated=2,
+    m = 5000
+    X, y = make_classification(n_samples=m, n_features=15, n_informative=12, n_redundant=2, n_repeated=1,
                                n_classes=2, random_state=seed)
     return X, y
 
 
-def train_classifier(classifier, parameters, data, seed, k=10, plot=None):
+def train_classifier(name, classifier, parameters, data, seed, k=10, plot=False):
     """
     Train the given classifier using a 10-fold cross validation.
     The given hyper-parameters are chosen using a 5-fold cross validation.
+    :param name: Name of the classifier to train.
     :param classifier: Classifier to train.
     :param parameters: Hyper-parameters to choose from.
     :param data: Samples to use for the training and test.
     :param seed: Seed to use for the random generator, used for the k-fold split.
     :param k: Number of fold for the cross validation (default 10).
-    :param plot: If not None, a plot for the hyper-parameters will be generated.
+    :param plot: If True, a plot for the hyper-parameters will be generated.
     :return: Performances (tuple of accuracy, f1, roc_auc) of the classifiers
              trained with the best hyper-parameters for each k-fold.
     """
@@ -51,15 +53,17 @@ def train_classifier(classifier, parameters, data, seed, k=10, plot=None):
     # split the data for the 10-fold cross validation
     kf = KFold(n_splits=k, shuffle=True, random_state=seed)
     for i, (train_index, test_index) in enumerate(kf.split(X, y)):
+        print("Training {}... Fold {}".format(name, i + 1))
+
         # extract training and test data
         X_train, y_train = X[train_index], y[train_index]
         X_test, y_test = X[test_index], y[test_index]
 
         # tune the hyper-parameters (5-fold cross validation, F1 scoring function)
-        best_classifier = GridSearchCV(classifier, parameters, scoring='f1', cv=5, n_jobs=5, refit=True)
+        best_classifier = GridSearchCV(classifier, parameters, scoring="f1", cv=5, n_jobs=5, refit=True)
         best_classifier.fit(X_train, y_train)
 
-        # TODO: measure performances of the different hyper parameters
+        # store the classifier for the plot (tuning of hyper-parameter)
         folds.append(best_classifier)
 
         # measure the performances
@@ -69,10 +73,7 @@ def train_classifier(classifier, parameters, data, seed, k=10, plot=None):
         roc_auc.append(roc_auc_score(y_test, prediction))
 
     # plot the choice of the best hyper-parameters
-    if plot is not None:
-
-        # average for the 10-folds
-        # np.mean(np.array(list(map(lambda x: x.cv_results_['mean_test_score'], folds))), axis=0)
+    if plot is True:
 
         # extract the parameter
         if len(parameters.keys()) != 1:
@@ -82,21 +83,21 @@ def train_classifier(classifier, parameters, data, seed, k=10, plot=None):
 
         # extract the hyper-parameter values
         assert len(folds) == k
-        x_axis_values = folds[0].cv_results_['param_' + parameter_name]
+        x_axis_values = folds[0].cv_results_["param_" + parameter_name]
         for i in range(k):
-            np.testing.assert_array_equal(x_axis_values, folds[i].cv_results_['param_' + parameter_name])
+            np.testing.assert_array_equal(x_axis_values, folds[i].cv_results_["param_" + parameter_name])
 
         # extract the F1 scores
-        y_axis_values = list(map(lambda x: x.cv_results_['mean_test_score'], folds))
+        y_axis_values = list(map(lambda x: x.cv_results_["mean_test_score"], folds))
 
         # plot the graph
         for i in range(k):
-            plt.semilogx(x_axis_values, y_axis_values[i], label=i + 1)
-        plt.title(plot + " - tuning of the best value for " + parameter_name)
+            plt.semilogx(x_axis_values, y_axis_values[i], label="Fold " + str(i + 1))
+        plt.title(name + " - tuning of the best value for " + parameter_name)
         plt.xlabel(parameter_name + " parameter")
         plt.ylabel("F1 scores (on the test set)")
         plt.legend(loc=4)
-        plt.savefig(plot.lower().replace(" ", "_") + '.png')
+        plt.savefig(name.lower().replace(" ", "_") + ".png")
         plt.close()
 
     # return the performances
@@ -121,16 +122,16 @@ def print_performances(classifier, performances):
     columns = "   {:0.3f}  |  {:0.3f} |  {:0.3f} "
     row_format = "  {:2d}  |" + columns
     mean_format = " mean |" + columns
-    separator = '------+----------+--------+---------'
+    separator = "------+----------+--------+---------"
 
     # the classifier
-    print('\n-----------------------------------------')
-    print(' Performances for: ' + classifier)
-    print('-----------------------------------------\n')
+    print("\n-----------------------------------------")
+    print(" Performances for: " + classifier)
+    print("-----------------------------------------\n")
 
     # header
     print(separator)
-    print('   i  | accuracy |   f1   | roc auc ')
+    print("   i  | accuracy |   f1   | roc auc ")
     print(separator)
 
     # body
@@ -140,7 +141,7 @@ def print_performances(classifier, performances):
 
     # mean
     print(mean_format.format(np.mean(accuracy), np.mean(f1), np.mean(roc_auc)))
-    print(separator + '\n')
+    print(separator + "\n")
 
 
 def main():
@@ -150,34 +151,41 @@ def main():
     """
 
     # TODO: remove
-    k = 4
+    k = 10
 
     # TODO: read it from the parameters of the script
     # pick some random seed
-    seed = 17294
+    seed = 3
 
     # generate the samples to work with
     X, y = generate_data(seed)
 
     # train SVM
-    svm = SVC(kernel='rbf', class_weight='balanced')
-    svm_parameters = {'C': [0.01, 0.033, 0.1, 0.33, 1, 3.33, 10, 33, 100]}
-    svm_performances = train_classifier(svm, svm_parameters, data=(X, y), seed=seed, k=k, plot='SVM')
-    print_performances('SVM', svm_performances)
+    svm = SVC(kernel="rbf", class_weight="balanced")
+    svm_parameters = {"C": [0.01, 0.033, 0.1, 0.33, 1, 3.33, 10, 33, 100]}
+    svm_performances = train_classifier("SVM", svm, svm_parameters, data=(X, y), seed=seed, k=k, plot=True)
+    print_performances("SVM", svm_performances)
 
     # train Random Forest
     rf = RandomForestClassifier(random_state=seed)
-    rf_parameters = {'n_estimators': [10, 100, 1000]}
-    rf_performances = train_classifier(rf, rf_parameters, data=(X, y), seed=seed, k=k, plot='Random Forest')
-    print_performances('Random Forest', rf_performances)
+    rf_parameters = {"n_estimators": [10, 100, 1000]}
+    rf_performances = train_classifier("Random Forest", rf, rf_parameters, data=(X, y), seed=seed, k=k, plot=True)
+    print_performances("Random Forest", rf_performances)
 
     # train Naive Bayes
     nb = GaussianNB()
     nb_parameters = {}
-    nb_performances = train_classifier(nb, nb_parameters, data=(X, y), seed=seed, k=k)
-    print_performances('Naive Bayes', nb_performances)
+    nb_performances = train_classifier("Naive Bayes", nb, nb_parameters, data=(X, y), seed=seed, k=k, plot=False)
+    print_performances("Naive Bayes", nb_performances)
+
+    # train Neural Network
+    nn = MLPClassifier(hidden_layer_sizes=50, activation='logistic', random_state=seed)
+    nn_parameters = {"max_iter": [1000]}
+    nn_performances = train_classifier("Neural Network", nn, nn_parameters, data=(X, y), seed=seed, k=k, plot=False)
+    print_performances("Neural Network", nn_performances)
 
     # evaluate performances
+    print("TODO: summary of performances...")
 
 
 # Entry Point
